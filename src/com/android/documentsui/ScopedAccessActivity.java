8a5a9e63cacfd953cd05bd4a87bcde7d8d880d49
maxwen <max.weninger@gmail.com>
2018-12-21 13:51:24 +0100

    DocumentsUI: fix text color in delete alert dialog
    Change-Id: I428a010aca387cf5670f597e9d445cbfbe04be9b

--- src/com/android/documentsui/ScopedAccessActivity.java
+++ src/com/android/documentsui/ScopedAccessActivity.java
@@ -347,7 +347,7 @@ public class ScopedAccessActivity extends Activity {
             }
             final TextView messageField = (TextView) view.findViewById(R.id.message);
             messageField.setText(message);
-            mDialog = new AlertDialog.Builder(mActivity, R.style.Theme_AppCompat_Light_Dialog_Alert)
+            mDialog = new AlertDialog.Builder(mActivity)
                     .setView(view)
                     .setPositiveButton(R.string.allow, listener)
                     .setNegativeButton(R.string.deny, listener)
